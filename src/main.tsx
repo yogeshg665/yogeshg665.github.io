e6c73c235ce52130f38775b0ff9cd37e071b6cdb
Yogesh Gupta Gudipati (Centific Technologies Inc) <v-ygudipati@microsoft.com>
2026-06-21 14:45:58 -0700

    Remove layout toggle; immersive is the only layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Root from "./Root";
+import ImmersiveApp from "./immersive/ImmersiveApp";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Root />
+    <ImmersiveApp />
   </React.StrictMode>
 );
